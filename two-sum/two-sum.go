4b9d23c796b8f1f2bdff0f71fd91df76e202493d
Ryan Benson <benson.ryan@gmail.com>
2020-03-29 21:12:04 -0500

    Optimizes two-sum method

--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -1,40 +1,35 @@
-package twosum
+package main
 
 import "fmt"
 
 func main() {
 	numbers := []int{9, 1, 2, 3, 4, 5}
-	target := 5
+	target := 9
 	result := twoSum(numbers, target)
 	fmt.Println(result)
 }
 
-func twoSum(list []int, target int) [2]int {
-	var result [2]int
-	found := false
+func twoSum(list []int, target int) []int {
+	// hold our ints in a map so we can do a lookup to see if we have the number we need
+	numberMap := make(map[int]int)
 
-	for i, v := range list {
-		if v >= target {
-			continue
+	// go through our list and try to find the number we need
+	for i, num := range list {
+		// what number do we need? look that up
+		numberNeeded := target - num
+
+		// do we have the number mapped already?
+		numberNeededIndex, ok := numberMap[numberNeeded]
+		// if so, we have our answer, so we can short circuit and return now
+		if ok {
+			return []int{numberNeededIndex, i}
 		}
 
-		for k, val := range list {
-			if val >= target {
-				continue
-			}
-			sum := v + val
-
-			if sum == target {
-				result[0] = i
-				result[1] = k
-				found = true
-				break
-			}
-		}
-
-		if found == true {
-			break
-		}
+		// didn't get our number, add what number we had to our map
+		// maybe it can be used in a subsequent number
+		numberMap[num] = i
 	}
-	return result
+
+	// didn't find anything?
+	return []int{}
 }
