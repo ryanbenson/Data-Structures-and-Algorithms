905d36be3550feac78da026f8417462e228356c9
Ryan Benson <benson.ryan@gmail.com>
2020-04-01 17:01:21 -0500

    Adds start of mean median mode exercise

---
+++ mean-median-mode/mean-median-mode.go
@@ -0,0 +1,16 @@
+package meanmedianmode
+
+type meanMedianMode struct {
+	mean float64
+	median float64
+	mode int
+}
+
+func getMeanMedianMode(numbers []int) meanMedianMode {
+	// TODO: Implementation
+	return meanMedianMode{
+		mean: 1,
+		median: 12.5,
+		mode: 2,
+	}
+}
---
+++ mean-median-mode/mean-median-mode_test.go
@@ -0,0 +1,41 @@
+package meanmedianmode
+
+import "testing"
+
+func TestGetMeanMedianMode_evenNumberOfValues(t *testing.T) {
+	results := getMeanMedianMode([]int{9, 1, 2, 3, 4, 5})
+	expected := meanMedianMode{
+		mean: 4,
+		median: 4,
+		mode: 1,
+	}
+
+	if results.mean != expected.mean {
+		t.Errorf("Mean is incorrect, got: %d, want: %d.", results.mean, expected.mean)
+	}
+	if results.median != expected.median {
+		t.Errorf("Median int is incorrect, got: %v, want: %v.", results.median, expected.median)
+	}
+	if results.mode != expected.mode {
+		t.Errorf("Mode int is incorrect, got: %d, want: %d.", results.mode, expected.mode)
+	}
+}
+
+func TestGetMeanMedianMode_oddNumberOfValues(t *testing.T) {7 9 12 12 23 35 786
+	results := getMeanMedianMode([]int{23, 12, 786, 7, 35, 12, 9})
+	expected := meanMedianMode{
+		mean: 126,
+		median: 12,
+		mode: 21,
+	}
+
+	if results.mean != expected.mean {
+		t.Errorf("Mean is incorrect, got: %d, want: %d.", results.mean, expected.mean)
+	}
+	if results.median != expected.median {
+		t.Errorf("Median int is incorrect, got: %v, want: %v.", results.median, expected.median)
+	}
+	if results.mode != expected.mode {
+		t.Errorf("Mode int is incorrect, got: %d, want: %d.", results.mode, expected.mode)
+	}
+}
