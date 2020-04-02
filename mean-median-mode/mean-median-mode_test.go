514b258cb7a174846b1fcd81ff720b59c89d4e17
Ryan Benson <benson.ryan@gmail.com>
2020-04-02 10:35:25 -0500

    Adds tests for median

--- mean-median-mode/mean-median-mode_test.go
+++ mean-median-mode/mean-median-mode_test.go
@@ -6,7 +6,7 @@ func TestGetMeanMedianMode_evenNumberOfValues(t *testing.T) {
 	results := getMeanMedianMode([]int{9, 1, 2, 3, 4, 5})
 	expected := meanMedianMode{
 		mean:   4,
-		median: 4,
+		median: 3,
 		mode:   1,
 	}
 
@@ -33,10 +33,10 @@ func TestGetMeanMedianMode_oddNumberOfValues(t *testing.T) {
 		t.Errorf("Mean is incorrect, got: %v, want: %v.", results.mean, expected.mean)
 	}
 	if results.median != expected.median {
-		t.Errorf("Median int is incorrect, got: %v, want: %v.", results.median, expected.median)
+		t.Errorf("Median is incorrect, got: %v, want: %v.", results.median, expected.median)
 	}
 	if results.mode != expected.mode {
-		t.Errorf("Mode int is incorrect, got: %d, want: %d.", results.mode, expected.mode)
+		t.Errorf("Mode is incorrect, got: %d, want: %d.", results.mode, expected.mode)
 	}
 }
 
@@ -47,5 +47,25 @@ func TestMean(t *testing.T) {
 
 	if results != expected {
 		t.Errorf("Mean is incorrect, got: %v, want: %v.", results, expected)
+	}
+}
+
+func TestMedian_EvenNumberItems(t *testing.T) {
+	results := median([]int{5, 7, 99, 5, 12, 34, 19, 357})
+	var expected float64
+	expected = 8
+
+	if results != expected {
+		t.Errorf("Median is incorrect, got: %v, want: %v.", results, expected)
+	}
+}
+
+func TestMedian_OddNumberItems(t *testing.T) {
+	results := median([]int{5, 7, 99, 5, 12, 34, 19})
+	var expected float64
+	expected = 5
+
+	if results != expected {
+		t.Errorf("Median is incorrect, got: %v, want: %v.", results, expected)
 	}
 }
