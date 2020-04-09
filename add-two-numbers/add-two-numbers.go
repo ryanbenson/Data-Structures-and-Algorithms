03edcf08df206a06ac1fadb56e8e23998f9e91c5
Ryan Benson <benson.ryan@gmail.com>
2020-04-08 19:32:40 -0500

    Adds new exercise for adding two link lists together

---
+++ add-two-numbers/add-two-numbers.go
@@ -0,0 +1,14 @@
+package addtwonumbers
+
+type listNode struct {
+	num  int
+	next *listNode
+}
+
+// Adds two list nodes togehter that are linked lists with an int in it
+func addTwoNumbers(list1 listNode, list2 listNode) listNode {
+	return listNode{
+		num:  1,
+		next: nil,
+	}
+}
