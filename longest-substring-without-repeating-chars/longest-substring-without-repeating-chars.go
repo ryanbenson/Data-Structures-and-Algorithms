c741a698fd3dccd0b4675aefe073b04f0184a53e
Ryan Benson <benson.ryan@gmail.com>
2020-04-21 17:53:56 -0500

    Creates sliding window

--- longest-substring-without-repeating-chars/longest-substring-without-repeating-chars.go
+++ longest-substring-without-repeating-chars/longest-substring-without-repeating-chars.go
@@ -1,9 +1,33 @@
 package longestsubstringwithoutrepeatingchars
 
+import "fmt"
+
 func getLongestSubstrLengthWithoutRepeats(haystack string) int {
 	// don't need to bother figuring it out if it's a single string
-	if len(haystack) == 1 {
+	haystackLen := len(haystack)
+	if haystackLen == 1 {
 		return 1
 	}
-	return 0
+
+	startingCharIndex := 0
+	charIndex := 0
+	for startingCharIndex != haystackLen -1 {
+
+		fmt.Println(haystack[startingCharIndex:charIndex])
+		charIndex += 1
+
+		// TODO: check if string we get from haystack is unique list of chars or not
+
+		if charIndex >= haystackLen -1 {
+			startingCharIndex += 1
+			// always reset the charindex to our starting index, not 0
+			charIndex = startingCharIndex
+		}
+
+		if startingCharIndex >= haystackLen -1 {
+			break
+		}
+	}
+
+	return longestSubstringLen
 }
