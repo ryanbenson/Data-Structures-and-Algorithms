2eea5dd9437c233302763a66c8ef8eeae0ae12a0
Ryan Benson <benson.ryan@gmail.com>
2020-04-27 18:37:41 -0500

    Adds tests for optimized version of median two arrs

--- median-two-sorted-arrays/median-two-sorted-arrays_test.go
+++ median-two-sorted-arrays/median-two-sorted-arrays_test.go
@@ -33,3 +33,33 @@ func TestGetMedianTwoArrays_oneValue(t *testing.T) {
 		t.Errorf("Mean is incorrect, got: %v, want: %v.", results, expected)
 	}
 }
+
+func TestGetMedianTwoArraysOptimized_evenNumberOfTotalValues(t *testing.T) {
+	results := getMedianTwoArraysOptimized([]int{1, 2, 3, 4, 5, 9}, []int{1, 2, 3, 4, 5, 7})
+	var expected float64
+	expected = 3
+
+	if results != expected {
+		t.Errorf("Mean is incorrect, got: %v, want: %v.", results, expected)
+	}
+}
+
+func TestGetMedianTwoArraysOptimized_oddNumberOfTotalValues(t *testing.T) {
+	results := getMedianTwoArraysOptimized([]int{1, 2, 3, 4, 5, 9, 11}, []int{1, 2, 3, 4, 5, 7})
+	var expected float64
+	expected = 4
+
+	if results != expected {
+		t.Errorf("Mean is incorrect, got: %v, want: %v.", results, expected)
+	}
+}
+
+func TestGetMedianTwoArraysOptimized_oneValue(t *testing.T) {
+	results := getMedianTwoArraysOptimized([]int{7}, []int{})
+	var expected float64
+	expected = 7
+
+	if results != expected {
+		t.Errorf("Mean is incorrect, got: %v, want: %v.", results, expected)
+	}
+}
