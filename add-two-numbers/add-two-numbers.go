a19082523c93cf46eb0ce6d750d34007483769db
Ryan Benson <benson.ryan@gmail.com>
2020-04-27 16:51:00 -0500

    Adds comment for function

--- add-two-numbers/add-two-numbers.go
+++ add-two-numbers/add-two-numbers.go
@@ -5,7 +5,7 @@ type listNode struct {
 	next *listNode
 }
 
-// Adds two list nodes togehter that are linked lists with an int in it
+// addTwoNumbers adds two list nodes togehter that are linked lists with an int in it
 func addTwoNumbers(list1 *listNode, list2 *listNode) *listNode {
 	// our list
 	head := &listNode{}
