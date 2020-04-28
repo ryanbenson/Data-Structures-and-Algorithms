584e5beede08baba342008cfb81733433db8b07f
Ryan Benson <benson.ryan@gmail.com>
2020-04-28 09:44:51 -0500

    Fixes tests on median two sorted, adds extra right side test

--- median-two-sorted-arrays/median-two-sorted-arrays_test.go
+++ median-two-sorted-arrays/median-two-sorted-arrays_test.go
@@ -24,16 +24,6 @@ func TestGetMedianTwoArrays_oddNumberOfTotalValues(t *testing.T) {
 	}
 }
 
-func TestGetMedianTwoArrays_oneValue(t *testing.T) {
-	results := getMedianTwoArrays([]int{7}, []int{})
-	var expected float64
-	expected = 7
-
-	if results != expected {
-		t.Errorf("Mean is incorrect, got: %v, want: %v.", results, expected)
-	}
-}
-
 func TestGetMedianTwoArraysOptimized_evenNumberOfTotalValues(t *testing.T) {
 	results := getMedianTwoArraysOptimized([]int{1, 2, 3, 4, 5, 9}, []int{1, 2, 3, 4, 5, 7})
 	var expected float64
@@ -54,12 +44,25 @@ func TestGetMedianTwoArraysOptimized_oddNumberOfTotalValues(t *testing.T) {
 	}
 }
 
-func TestGetMedianTwoArraysOptimized_oneValue(t *testing.T) {
-	results := getMedianTwoArraysOptimized([]int{7}, []int{})
+func TestGetMedianTwoArraysOptimized_oddNumberOfTotalValuesRightSide(t *testing.T) {
+	results := getMedianTwoArraysOptimized([]int{9, 99, 999, 9999, 99999}, []int{1, 1, 1, 1, 1, 2, 3, 4, 5, 7})
 	var expected float64
-	expected = 7
+	expected = 4
 
 	if results != expected {
 		t.Errorf("Mean is incorrect, got: %v, want: %v.", results, expected)
 	}
+}
+
+// Benchmarking the unoptimized and optimized
+func BenchmarkGetMedianTwoArrays(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        getMedianTwoArrays([]int{1, 2, 3, 4, 5, 9}, []int{1, 2, 3, 4, 5, 7})
+    }
+}
+
+func BenchmarkGetMedianTwoArraysOptimized(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        getMedianTwoArraysOptimized([]int{1, 2, 3, 4, 5, 9}, []int{1, 2, 3, 4, 5, 7})
+    }
 }
