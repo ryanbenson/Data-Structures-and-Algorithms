2ee854ad2f473b97e30f77b5ea07da8320adad69
Ryan Benson <benson.ryan@gmail.com>
2020-05-07 13:55:09 -0500

    Creates keymap for keys and rows

--- one-row/one-row.go
+++ one-row/one-row.go
@@ -1,5 +1,32 @@
 package onerow
 
 func isOneRowFilter(sequence []string) []string {
-	return []string{"test"}
+	keyMap := map[rune]int {
+		'q': 1,
+		'w': 1,
+		'e': 1,
+		'r': 1,
+		't': 1,
+		'y': 1,
+		'u': 1,
+		'i': 1,
+		'o': 1,
+		'p': 1,
+		'a': 2,
+		's': 2,
+		'd': 2,
+		'f': 2,
+		'g': 2,
+		'h': 2
+		'j': 2,
+		'k': 2,
+		'l': 2,
+		'z': 2,
+		'x': 2,
+		'c': 2,
+		'v': 2,
+		'b': 2,
+		'n': 2,
+		'm': 2
+	}
 }
