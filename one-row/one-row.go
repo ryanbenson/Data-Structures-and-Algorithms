a084a7e8c3122b5bdcf09b6af82ddd749b6fce2b
Ryan Benson <benson.ryan@gmail.com>
2020-05-07 14:04:54 -0500

    Adds solution for one row

--- one-row/one-row.go
+++ one-row/one-row.go
@@ -1,6 +1,18 @@
 package onerow
 
-func isOneRowFilter(sequence []string) []string {
+func isOneRowFilter(words []string) []string {
+	wordsWithSameRow := make([]string, 0)
+
+	for _, word := range words {
+		isOneRow := isWordOneRow(word)
+		if isOneRow == true {
+			wordsWithSameRow = append(wordsWithSameRow, word)
+		}
+	}
+	return wordsWithSameRow
+}
+
+func isWordOneRow(word string) bool {
 	keyMap := map[rune]int {
 		'q': 1,
 		'w': 1,
@@ -17,16 +29,33 @@ func isOneRowFilter(sequence []string) []string {
 		'd': 2,
 		'f': 2,
 		'g': 2,
-		'h': 2
+		'h': 2,
 		'j': 2,
 		'k': 2,
 		'l': 2,
-		'z': 2,
-		'x': 2,
-		'c': 2,
-		'v': 2,
-		'b': 2,
-		'n': 2,
-		'm': 2
+		'z': 3,
+		'x': 3,
+		'c': 3,
+		'v': 3,
+		'b': 3,
+		'n': 3,
+		'm': 3,
 	}
+
+	row := 0
+	for i, letter := range word {
+		if i == 0 {
+			rowNum, ok := keyMap[letter]
+			if !ok {
+				return false
+			}
+			row = rowNum
+			continue
+		}
+		rowNum, ok := keyMap[letter]
+		if !ok || rowNum != row {
+			return false
+		}
+	}
+	return true
 }
