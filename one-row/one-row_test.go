b07f2c4da2f01be4b05c4a426279ceb817c06a5e
Ryan Benson <benson.ryan@gmail.com>
2020-05-08 16:56:08 -0500

    Adds more tests for one row

--- one-row/one-row_test.go
+++ one-row/one-row_test.go
@@ -8,6 +8,46 @@ func TestIsOneRowFilter_HasSome(t *testing.T) {
 	expected := []string{"pop", "lag", "typewriter"}
 
 	if len(result) != len(expected) {
-		t.Errorf("When testing a list of words that is is incorrect, got: %v, want: %v.", result, expected)
+		t.Errorf("When testing a list of words that has some valid is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestIsOneRowFilter_BadCharIncluded(t *testing.T) {
+	words := []string{"!candy", "dood...le", "po()p", "shield", "lag", "typewriter"}
+	result := isOneRowFilter(words)
+	expected := []string{"lag", "typewriter"}
+
+	if len(result) != len(expected) {
+		t.Errorf("When testing a list of words that has some valid with bad chars is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestIsOneRowFilter_Empty(t *testing.T) {
+	words := []string{}
+	result := isOneRowFilter(words)
+	expected := []string{}
+
+	if len(result) != len(expected) {
+		t.Errorf("When testing a list of words that is empty is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestIsWordOneRow_Valid(t *testing.T) {
+	word := "typewriter"
+	result := isWordOneRow(word)
+	expected := true
+
+	if result != expected {
+		t.Errorf("When testing a word that is valid is incorrect, got %v, want: %v", result, expected)
+	}
+}
+
+func TestIsWordOneRow_Invalid(t *testing.T) {
+	word := "hello"
+	result := isWordOneRow(word)
+	expected := false
+
+	if result != expected {
+		t.Errorf("When testing a word that is valid is incorrect, got %v, want: %v", result, expected)
 	}
 }
