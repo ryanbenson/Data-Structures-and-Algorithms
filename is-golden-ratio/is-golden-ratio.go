cfa27775a49bbe671e6768d0271ac0f517cf61fc
Ryan Benson <benson.ryan@gmail.com>
2020-05-14 09:27:18 -0500

    Adds solution for is golden ratio

--- is-golden-ratio/is-golden-ratio.go
+++ is-golden-ratio/is-golden-ratio.go
@@ -1,5 +1,19 @@
 package isgoldenratio
 
+import "math"
+
 func isGoldenRatio(x float64, y float64) bool {
-	return true
+	a := x
+	b := y
+	if y > x {
+		a = y
+		b = x
+	}
+	left := floatRemainder((a + b) / a)
+	right := floatRemainder(a / b)
+	return left == right
+}
+
+func floatRemainder(f float64) float64 {
+	return math.Floor(f*1000) / 1000
 }
