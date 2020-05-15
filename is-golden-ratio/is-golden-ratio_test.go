55d105620b29d849f9ae7d199de4c89a48cc5c01
Ryan Benson <benson.ryan@gmail.com>
2020-05-15 13:43:43 -0500

    Adds tests for getting float rounded

--- is-golden-ratio/is-golden-ratio_test.go
+++ is-golden-ratio/is-golden-ratio_test.go
@@ -47,6 +47,42 @@ func TestIsGoldenRatio_ValidThree(t *testing.T) {
 	}
 }
 
+func TestFloatRemainder_NoRemainder(t *testing.T) {
+	result := floatRemainder(1.0)
+	expected := 1.000
+
+	if result != expected {
+		t.Errorf("When rounding float with remainder: is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestFloatRemainder_RoundDown(t *testing.T) {
+	result := floatRemainder(13.1114999)
+	expected := 13.111
+
+	if result != expected {
+		t.Errorf("When rounding float with remainder: is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestFloatRemainder_RoundUpDecimal(t *testing.T) {
+	result := floatRemainder(13.444999)
+	expected := 13.445
+
+	if result != expected {
+		t.Errorf("When rounding float with remainder: is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestFloatRemainder_RoundUpEnd(t *testing.T) {
+	result := floatRemainder(13.999999)
+	expected := 14.000
+
+	if result != expected {
+		t.Errorf("When rounding float with remainder: is incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
 func BenchmarkIsGoldenRatio(b *testing.B) {
     for i := 0; i < b.N; i++ {
 		isGoldenRatio(19.416, 12)
