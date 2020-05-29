fd6feb065113e46fa41c99b463fe53799cb10446
Ryan Benson <benson.ryan@gmail.com>
2020-05-29 14:56:08 -0500

    Adds base functions for basic twitter

--- basic-twitter/basic-twitter.go
+++ basic-twitter/basic-twitter.go
@@ -10,4 +10,17 @@ type tweet struct {
 }
 
 func getNewsFeed(userID int) []tweet {
+  return []tweet{}
+}
+
+func postTweet(userID int, post tweet) bool {
+  return true
+}
+
+func follow(userID int, followerUserID int) bool {
+  return true
+}
+
+func unfollow(userID int, followerUserID int) bool {
+  return true
 }
