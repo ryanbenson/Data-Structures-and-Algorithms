8b600da1f44e85dfc1712800e15a7073bec9b2ba
Ryan Benson <benson.ryan@gmail.com>
2020-06-01 18:24:51 -0500

    Adds basic data checking for IDs and self

--- basic-twitter/basic-twitter.go
+++ basic-twitter/basic-twitter.go
@@ -19,14 +19,34 @@ func getNewsFeed(userID int) []tweet {
   return []tweet{}
 }
 
-func postTweet(userID int, post tweet) bool {
+func postTweet(userID int, post tweet) (bool, error) {
+  if userID == nil {
+    return nil, errors.New("A user is required to post a tweet")
+  }
+
   return true
 }
 
-func follow(userID int, followerUserID int) bool {
+func follow(userID int, followerUserID int) (bool, error) {
+  if userID == nil {
+    return nil, errors.New("A user is follow another user")
+  }
+
+  if followerUserID == nil {
+    return nil, errors.New("A user to follow is follow another user")
+  }
+
+  if userID == followerUserID {
+    return nil, errors.New("Unable to follow yourself")
+  }
+
   return true
 }
 
-func unfollow(userID int, followerUserID int) bool {
+func unfollow(userID int, followerUserID int) (bool, error) {
+  if userID == followerUserID {
+    return nil, errors.New("Unable to unfollow yourself")
+  }
+
   return true
 }
