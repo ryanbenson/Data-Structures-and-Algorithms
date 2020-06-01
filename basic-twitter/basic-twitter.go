b86f2cb840e685ac64977873b22980cff47be2c7
Ryan Benson <benson.ryan@gmail.com>
2020-06-01 18:28:33 -0500

    Fixes id check and returned bool on errors

--- basic-twitter/basic-twitter.go
+++ basic-twitter/basic-twitter.go
@@ -23,24 +23,24 @@ func getNewsFeed(userID int) []tweet {
 }
 
 func postTweet(userID int, post tweet) (bool, error) {
-  if userID == nil {
-    return nil, errors.New("A user is required to post a tweet")
+  if userID == 0 {
+    return false, errors.New("A user is required to post a tweet")
   }
 
   return true
 }
 
 func follow(userID int, followerUserID int) (bool, error) {
-  if userID == nil {
-    return nil, errors.New("A user is follow another user")
+  if userID == 0 {
+    return false, errors.New("A user is follow another user")
   }
 
-  if followerUserID == nil {
-    return nil, errors.New("A user to follow is follow another user")
+  if followerUserID == 0 {
+    return false, errors.New("A user to follow is follow another user")
   }
 
   if userID == followerUserID {
-    return nil, errors.New("Unable to follow yourself")
+    return false, errors.New("Unable to follow yourself")
   }
 
   return true
@@ -48,7 +48,7 @@ func follow(userID int, followerUserID int) (bool, error) {
 
 func unfollow(userID int, followerUserID int) (bool, error) {
   if userID == followerUserID {
-    return nil, errors.New("Unable to unfollow yourself")
+    return false, errors.New("Unable to unfollow yourself")
   }
 
   return true
