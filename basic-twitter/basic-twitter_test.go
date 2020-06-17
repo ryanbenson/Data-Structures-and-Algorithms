99f0e29bd558581233d84698941f3ef0d1886ac5
Ryan Benson <benson.ryan@gmail.com>
2020-06-17 09:59:11 -0500

    Adds more tests for unfollowing

--- basic-twitter/basic-twitter_test.go
+++ basic-twitter/basic-twitter_test.go
@@ -113,3 +113,63 @@ func TestUnfollow_InvalidSelfIDs(t *testing.T) {
 		t.Errorf("When unfollowing a user that is yourself , an error should have been given, got: %v, want: %v.", err, nil)
 	}
 }
+
+func TestUnfollow_NoUserIDMatch(t *testing.T) {
+	followerID := 1
+	userID := 890
+
+	result, err := unfollow(userID, followerID)
+	expected := false
+
+	if result != expected {
+		t.Errorf("When unfollowing a user when the user does not exist, got: %v, want: %v.", result, expected)
+	}
+
+	if err == nil {
+		t.Errorf("When unfollowing a user when the user does not exist, an error should have been given, got: %v, want: %v.", err, nil)
+	}
+}
+
+func TestUnfollow_NotFollowingUserID(t *testing.T) {
+	followerID := 1
+	userID := 3
+
+	newUser := &user{
+		userID: userID,
+		following: []int{2},
+	}
+	users = append(users, newUser)
+	
+	result, err := unfollow(userID, followerID)
+	expected := false
+
+	if result != expected {
+		t.Errorf("When unfollowing a user but you are not following that user, got: %v, want: %v.", result, expected)
+	}
+
+	if err == nil {
+		t.Errorf("When unfollowing a user but you are not following that user, an error should have been given, got: %v, want: %v.", err, nil)
+	}
+}
+
+func TestUnfollow(t *testing.T) {
+	followerID := 9
+	userID := 4
+
+	newUser := &user{
+		userID: userID,
+		following: []int{followerID},
+	}
+	users = append(users, newUser)
+	
+	result, err := unfollow(userID, followerID)
+	expected := true
+
+	if result != expected {
+		t.Errorf("When unfollowing a user, got: %v, want: %v.", result, expected)
+	}
+
+	if err != nil {
+		t.Errorf("When unfollowing a user, an error should not have been given, got: %v, want: %v.", err, nil)
+	}
+}
