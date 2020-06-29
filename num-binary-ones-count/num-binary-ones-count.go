3c28ac883d0d4aa11070a8c3297ef77c4d953ebe
Ryan Benson <benson.ryan@gmail.com>
2020-06-29 17:01:29 -0500

    Adds function comment for num binary ones count

--- num-binary-ones-count/num-binary-ones-count.go
+++ num-binary-ones-count/num-binary-ones-count.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+// getBinaryOnesCount determines how many 1 characters
+// are in the binary representation of a number
 func getBinaryOnesCount(num int64) int {
 	binaryOfInt := strconv.FormatInt(num, 2)
 	return strings.Count(binaryOfInt, "1")
