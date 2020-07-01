4afe3410cb2c8638590f3843db4ddc6087dd12cf
Ryan Benson <benson.ryan@gmail.com>
2020-07-01 16:41:53 -0500

    Adds new exercise for rock paper scissors

---
+++ rock-paper-scissors/rock-paper-scissors.go
@@ -0,0 +1,13 @@
+package rockpaperscissors
+
+func getOpponentMove() string {
+	return "rock"
+}
+
+func isValidMove(move string) bool {
+	return true
+}
+
+func evaluateGame(userMove string, opponentMove string) string {
+	return "win"
+}
---
+++ rock-paper-scissors/rock-paper-scissors_test.go
@@ -0,0 +1,64 @@
+package rockpaperscissors
+
+import (
+	"testing"
+)
+
+func TestGetOpponentMove(t *testing.T) {
+	move := getOpponentMove()
+
+	if move != "rock" && move != "paper" && move != "scissors" {
+		t.Errorf("When getting a random choice, is it not a valid move, got: %v.", move)
+	}
+}
+
+func TestIsValidMove_IsValid(t *testing.T) {
+	move := "scissors"
+	isValid := isValidMove(move)
+
+	if isValid != true {
+		t.Errorf("When determining if a move is valid, it was incorrect, got: %v, want: %v.", isValid, true)
+	}
+}
+
+func TestIsValidMove_IsInvalid(t *testing.T) {
+	move := "lizard"
+	isValid := isValidMove(move)
+
+	if isValid != false {
+		t.Errorf("When determining if a move is valid, it was incorrect, got: %v, want: %v.", isValid, false)
+	}
+}
+
+func TestEvaluateGame_Win(t *testing.T) {
+	userMove := "rock"
+	opponentMove := "paper"
+	result := evaluateGame(userMove, opponentMove)
+	expected := "win"
+
+	if result != "win" {
+		t.Errorf("When determining a winner, and should win, it was incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestEvaluateGame_Lose(t *testing.T) {
+	userMove := "paper"
+	opponentMove := "rock"
+	result := evaluateGame(userMove, opponentMove)
+	expected := "lose"
+
+	if result != "lose" {
+		t.Errorf("When determining a winner, and should lose, it was incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+
+func TestEvaluateGame_Tie(t *testing.T) {
+	userMove := "rock"
+	opponentMove := "rock"
+	result := evaluateGame(userMove, opponentMove)
+	expected := "tie"
+
+	if result != "tie" {
+		t.Errorf("When determining a winner, and should tie, it was incorrect, got: %v, want: %v.", result, expected)
+	}
+}
