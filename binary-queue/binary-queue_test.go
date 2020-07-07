6e1d9d44eed1e0a6553ce53aebfa6d43cfe568ef
Ryan Benson <benson.ryan@gmail.com>
2020-07-07 10:44:07 -0500

    Fixes binary queue test to ensure all expected values are found

--- binary-queue/binary-queue_test.go
+++ binary-queue/binary-queue_test.go
@@ -9,16 +9,21 @@ func TestBinaryQueue_Success(t *testing.T) {
 	result := binaryQueue(num)
 	expected := []int{1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111, 10000}
 
-	for _, resultVal := range result {
+	allMatched := true
+	for _, resultVal := range expected {
 		found := false
-		for _, expectedVal := range expected {
+		for _, expectedVal := range result {
 			if resultVal == expectedVal {
 				found = true
 			}
 		}
-		// anything not found? something went wrong
+		// if any fail, then it will error out
 		if found == false {
-			t.Errorf("When getting a binary queue, incorrect queue given, got: %v, expected: %v", result, expected)
+			allMatched = false
 		}
+	}
+	// anything not found? something went wrong
+	if allMatched == false {
+		t.Errorf("When getting a binary queue, incorrect queue given, got: %v, expected: %v", result, expected)
 	}
 }
