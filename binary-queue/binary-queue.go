d09525deb44055e7c7201984a524dee66a526f7a
Ryan Benson <benson.ryan@gmail.com>
2020-07-07 10:52:41 -0500

    Adds implementation of binary queue

--- binary-queue/binary-queue.go
+++ binary-queue/binary-queue.go
@@ -1,5 +1,17 @@
 package binaryqueue
 
-func binaryQueue(lastNum int) []int {
-	return []int{1, 10}
+import "strconv"
+
+func binaryQueue(lastNum int) []string {
+	queue := []string{}
+	if lastNum <= 0 {
+		return queue
+	}
+
+	for i := 1; i <= lastNum; i++ {
+		// need to use int64 to get binary representation of int
+		binaryOfInt := strconv.FormatInt(int64(i), 2)
+		queue = append(queue, binaryOfInt)
+	}
+	return queue
 }
