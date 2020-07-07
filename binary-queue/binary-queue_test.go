85aa7fd29cdc45d0349f352c1463c63c5c24404b
Ryan Benson <benson.ryan@gmail.com>
2020-07-07 10:52:54 -0500

    Updates tests for binary queue

--- binary-queue/binary-queue_test.go
+++ binary-queue/binary-queue_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 func TestBinaryQueue_Success(t *testing.T) {
-	num := 10
+	num := 16
 	result := binaryQueue(num)
-	expected := []int{1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111, 10000}
+	expected := []string{"1", "10", "11", "100", "101", "110", "111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111", "10000"}
 
 	allMatched := true
 	for _, resultVal := range expected {
@@ -24,6 +24,16 @@ func TestBinaryQueue_Success(t *testing.T) {
 	}
 	// anything not found? something went wrong
 	if allMatched == false {
+		t.Errorf("When getting a binary queue, incorrect queue given, got: %v, expected: %v", result, expected)
+	}
+}
+func TestBinaryQueue_Zero(t *testing.T) {
+	num := 0
+	result := binaryQueue(num)
+	expected := []string{}
+
+	// got anything? if so, it should be invalid
+	if len(result) > 0 {
 		t.Errorf("When getting a binary queue, incorrect queue given, got: %v, expected: %v", result, expected)
 	}
 }
