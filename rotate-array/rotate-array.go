998090c243b1614617df6dc5170619fc2c2179de
Ryan Benson <benson.ryan@gmail.com>
2020-07-10 11:43:07 -0500

    Fixes implementation and tests for rotate array

--- rotate-array/rotate-array.go
+++ rotate-array/rotate-array.go
@@ -1,13 +1,24 @@
 package rotatearray
 
-func rotateArray(arr []int, nthNum int) []int {
-	index := nthNum + 1 // use array based, so +1 the nth num
-	firstChunk := arr[:index]
-	secondChunk := arr[index:]
+import (
+	"errors"
+)
+
+func rotateArray(arr []int, index int) ([]int, error) {
+	if (index + 1) == len(arr) {
+		return arr, nil
+	}
+
+	if index > len(arr) {
+		return nil, errors.New("Number provided exceeds length of array")
+	}
+
+	firstChunk := arr[:index+1]
+	secondChunk := arr[index+1:]
 
 	var rotatedArr []int
 	rotatedArr = append(rotatedArr, secondChunk...)
 	rotatedArr = append(rotatedArr, firstChunk...)
 
-	return rotatedArr
+	return rotatedArr, nil
 }
--- rotate-array/rotate-array_test.go
+++ rotate-array/rotate-array_test.go
@@ -7,7 +7,7 @@ import (
 func TestRotateArray_Success(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5}
 	index := 2
-	result := rotateArray(arr, index)
+	result, err := rotateArray(arr, index)
 
 	expected := []int{4, 5, 1, 2, 3}
 
@@ -22,5 +22,66 @@ func TestRotateArray_Success(t *testing.T) {
 	// anything not found? something went wrong
 	if allMatched == false {
 		t.Errorf("When rotating an array, incorrect queue given, got: %v, expected: %v", result, expected)
+	}
+	if err != nil {
+		t.Errorf("When rotating an array, incorrect error given, got: %v, expected: %v", result, nil)
+	}
+}
+func TestRotateArray_FirstNum(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5}
+	index := 0
+	result, err := rotateArray(arr, index)
+
+	expected := []int{2, 3, 4, 5, 1}
+
+	allMatched := true
+	for i, expectedVal := range expected {
+		resultVal := result[i]
+		// if any fail, then it will error out
+		if expectedVal != resultVal {
+			allMatched = false
+		}
+	}
+	// anything not found? something went wrong
+	if allMatched == false {
+		t.Errorf("When rotating an array, incorrect queue given, got: %v, expected: %v", result, expected)
+	}
+	if err != nil {
+		t.Errorf("When rotating an array, incorrect error given, got: %v, expected: %v", result, nil)
+	}
+}
+func TestRotateArray_SuccessLastIndex(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5}
+	index := 4
+	result, err := rotateArray(arr, index)
+
+	expected := []int{1, 2, 3, 4, 5}
+
+	allMatched := true
+	for i, expectedVal := range expected {
+		resultVal := result[i]
+		// if any fail, then it will error out
+		if expectedVal != resultVal {
+			allMatched = false
+		}
+	}
+	// anything not found? something went wrong
+	if allMatched == false {
+		t.Errorf("When rotating an array, incorrect queue given, got: %v, expected: %v", result, expected)
+	}
+	if err != nil {
+		t.Errorf("When rotating an array, incorrect error given, got: %v, expected: %v", result, nil)
+	}
+}
+func TestRotateArray_IndexExceedsBounds(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5}
+	index := 90
+	result, err := rotateArray(arr, index)
+
+	if result != nil {
+		t.Errorf("When rotating an array with a too high index, incorrect queue given, got: %v, expected: %v", result, nil)
+	}
+	if err == nil {
+		t.Errorf("When rotating an array with a too high index, incorrect error given, got: %v, expected: %v", result, nil)
 	}
 }
