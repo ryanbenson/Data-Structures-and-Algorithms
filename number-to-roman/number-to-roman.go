e2420d383f7b0e01b56f8c32a5e6a75c3f2bbf9f
Ryan Benson <benson.ryan@gmail.com>
2020-07-22 16:11:49 -0500

    Adds conversion table for roman numerals

--- number-to-roman/number-to-roman.go
+++ number-to-roman/number-to-roman.go
@@ -1,5 +1,23 @@
 package numbertoroman
 
 func fromInt(number int) string {
+	conversions := []struct {
+		value int
+		digit string
+	}{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
 	return "XIII"
 }
