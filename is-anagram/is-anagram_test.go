133a43dbde8b3622ebf4ebbd44ad605b94499064
Ryan Benson <benson.ryan@gmail.com>
2020-07-29 13:03:07 -0500

    Adds more tests for checking anagram

--- is-anagram/is-anagram_test.go
+++ is-anagram/is-anagram_test.go
@@ -4,11 +4,30 @@ import (
 	"testing"
 )
 
-func TestisAnagram_Success(t *testing.T) {
-	result := isAnagram("fried", "fired")
+func TestIsAnagram_Success(t *testing.T) {
+	result1 := isAnagram("fried", "fired")
+	result2 := isAnagram("listen", "silent")
 	expected := true
 
-	if result != expected {
-		t.Errorf("When determing anagram, incorrect result given, got: %v, expected: %v", result, expected)
+	if result1 != expected {
+		t.Errorf("When determing anagram 1, incorrect result given, got: %v, expected: %v", result1, expected)
+	}
+
+	if result2 != expected {
+		t.Errorf("When determing anagram 2, incorrect result given, got: %v, expected: %v", result2, expected)
+	}
+}
+
+func TestIsAnagram_Fail(t *testing.T) {
+	result1 := isAnagram("bob", "boo")
+	result2 := isAnagram("hello", "help")
+	expected := false
+
+	if result1 != expected {
+		t.Errorf("When determing anagram 1, incorrect result given, got: %v, expected: %v", result1, expected)
+	}
+
+	if result2 != expected {
+		t.Errorf("When determing anagram 2, incorrect result given, got: %v, expected: %v", result2, expected)
 	}
 }
