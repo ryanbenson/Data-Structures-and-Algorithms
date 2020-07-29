8e14f694f456476ec259d106fc6f10f2db076b98
Ryan Benson <benson.ryan@gmail.com>
2020-07-29 13:03:24 -0500

    Adds basic implementation of checking anagram

--- is-anagram/is-anagram.go
+++ is-anagram/is-anagram.go
@@ -1,5 +1,18 @@
 package isanagram
 
+import (
+	"sort"
+	"strings"
+)
+
 func isAnagram(str1 string, str2 string) bool {
-	return true
+	sortedStr1 := sortString(str1)
+	sortedStr2 := sortString(str2)
+	return sortedStr1 == sortedStr2
+}
+
+func sortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
 }
