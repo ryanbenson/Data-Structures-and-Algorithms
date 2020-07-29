492d0c7cd91df808be65f9b8b758f43e2af84fe7
Ryan Benson <benson.ryan@gmail.com>
2020-07-29 13:04:21 -0500

    Adds benchmark for is anagram sorting

--- is-anagram/is-anagram_test.go
+++ is-anagram/is-anagram_test.go
@@ -31,3 +31,9 @@ func TestIsAnagram_Fail(t *testing.T) {
 		t.Errorf("When determing anagram 2, incorrect result given, got: %v, expected: %v", result2, expected)
 	}
 }
+
+func BenchmarkIsAnagram_SortStrings(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		isAnagram("listen", "silent")
+	}
+}
