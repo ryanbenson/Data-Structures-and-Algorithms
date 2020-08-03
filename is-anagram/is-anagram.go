783521237541aad4c7d714c41ca589e5523b2c3b
Ryan Benson <benson.ryan@gmail.com>
2020-08-03 15:55:17 -0500

    Working on loop and letter checking in is anagram

--- is-anagram/is-anagram.go
+++ is-anagram/is-anagram.go
@@ -11,6 +11,27 @@ func isAnagram(str1 string, str2 string) bool {
 	return sortedStr1 == sortedStr2
 }
 
+func isAnagramLoop(str1 string, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	if str1 == "" && str2 == "" {
+		return true
+	}
+
+	strLen := len(str1)
+	for i := 0; i < strLen; i++ {
+		letter := str1[i]
+		str1Index := strings.IndexByte(str1, letter)
+		str2Index := strings.IndexByte(str2, letter)
+
+		if str1Index == -1 || str2Index == -1 {
+			return false
+		}
+	}
+}
+
 func sortString(w string) string {
 	s := strings.Split(w, "")
 	sort.Strings(s)
