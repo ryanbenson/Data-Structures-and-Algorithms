cb950a6cce4e552b78699522525f40a43c107333
Ryan Benson <benson.ryan@gmail.com>
2020-08-04 09:48:15 -0500

    Finished is anagram version that manipulates strings

--- is-anagram/is-anagram.go
+++ is-anagram/is-anagram.go
@@ -11,7 +11,7 @@ func isAnagram(str1 string, str2 string) bool {
 	return sortedStr1 == sortedStr2
 }
 
-func isAnagramLoop(str1 string, str2 string) bool {
+func isAnagram_StringsManip(str1 string, str2 string) bool {
 	if len(str1) != len(str2) {
 		return false
 	}
@@ -20,9 +20,8 @@ func isAnagramLoop(str1 string, str2 string) bool {
 		return true
 	}
 
-	strLen := len(str1)
-	for i := 0; i < strLen; i++ {
-		letter := str1[i]
+	for len(str1) > 0 {
+		letter := str1[0]
 		str1Index := strings.IndexByte(str1, letter)
 		str2Index := strings.IndexByte(str2, letter)
 
@@ -30,7 +29,15 @@ func isAnagramLoop(str1 string, str2 string) bool {
 			return false
 		}
 
+		str1 = str1[:str1Index] + str1[str1Index+1:]
+		str2 = str2[:str2Index] + str2[str2Index+1:]
 	}
+
+	if len(str1) == 0 && len(str2) == 0 {
+		return true
+	}
+
+	return false
 }
 
 func sortString(w string) string {
