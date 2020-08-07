b6a7605688a163916b162e7984796067b37cfd58
Ryan Benson <benson.ryan@gmail.com>
2020-08-07 16:09:18 -0500

    Adds test for first dup letter that isn't sequential

--- get-first-duplicate-letter/get-first-duplicate-letter_test.go
+++ get-first-duplicate-letter/get-first-duplicate-letter_test.go
@@ -9,4 +9,11 @@ func TestGetFirstDuplicateLetter_Success(t *testing.T) {
 	if result != expected {
 		t.Errorf("When the first duplicate letter, incorrect result given, got: %v, expected: %v", result, expected)
 	}
+
+	result2 := getFirstDuplicateLetter("abcdefabc")
+	expected2 := "a"
+
+	if result2 != expected2 {
+		t.Errorf("When the first duplicate letter (2), incorrect result given, got: %v, expected: %v", result2, expected2)
+	}
 }
