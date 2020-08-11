5b16cba9180233c6d1305b2b526e4843f9433203
Ryan Benson <benson.ryan@gmail.com>
2020-08-11 09:11:52 -0500

    Adds benchmark for get first dup letter

--- get-first-duplicate-letter/get-first-duplicate-letter_test.go
+++ get-first-duplicate-letter/get-first-duplicate-letter_test.go
@@ -2,18 +2,24 @@ package getfirstduplicateletter
 
 import "testing"
 
-func TestGetFirstDuplicateLetter_Success(t *testing.T) {
-	result := getFirstDuplicateLetter("hello")
+func TestGetFirstDuplicateLetter_Map_Success(t *testing.T) {
+	result := getFirstDuplicateLetter_Map("hello")
 	expected := "l"
 
 	if result != expected {
 		t.Errorf("When the first duplicate letter, incorrect result given, got: %v, expected: %v", result, expected)
 	}
 
-	result2 := getFirstDuplicateLetter("abcdefabc")
+	result2 := getFirstDuplicateLetter_Map("abcdefabc")
 	expected2 := "a"
 
 	if result2 != expected2 {
 		t.Errorf("When the first duplicate letter (2), incorrect result given, got: %v, expected: %v", result2, expected2)
+	}
+}
+
+func BenchmarkGetFirstDuplicateLetter_Map(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		getFirstDuplicateLetter_Map("abcdefabc")
 	}
 }
