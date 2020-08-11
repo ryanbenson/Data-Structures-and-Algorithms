badd0d2b69e941eb464037ae20abeea96875c453
Ryan Benson <benson.ryan@gmail.com>
2020-08-11 09:12:04 -0500

    Adds map implementation for get first dupe letter

--- get-first-duplicate-letter/get-first-duplicate-letter.go
+++ get-first-duplicate-letter/get-first-duplicate-letter.go
@@ -1,5 +1,20 @@
 package getfirstduplicateletter
 
-func getFirstDuplicateLetter(word string) string {
+import "strings"
+
+func getFirstDuplicateLetter_Map(word string) string {
+	letters := strings.Split(word, "")
+
+	letterMap := make(map[string]int)
+
+	for _, letter := range letters {
+		_, ok := letterMap[letter]
+		if ok {
+			return letter
+		}
+		letterMap[letter] = 1
+	}
+
+	// never found one
 	return ""
 }
