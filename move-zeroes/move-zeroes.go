68b07a5ecd0ed94b704dedd51c2b3f291fb8a11a
Ryan Benson <benson.ryan@gmail.com>
2020-08-20 17:00:09 -0500

    Adds new implementation for moving zeroes working with arrays

--- move-zeroes/move-zeroes.go
+++ move-zeroes/move-zeroes.go
@@ -1,5 +1,6 @@
 package movezeroes
 
+// moves all zeroes to the end of an array
 func moveZeroes(numbers []int) []int {
 	numbersLen := len(numbers)
 	// this will make an array for as long as the numbers is,
@@ -15,6 +16,30 @@ func moveZeroes(numbers []int) []int {
 			index++
 		}
 	}
+
+	return finalNumbers
+}
+
+// moves all zeroes to the end of an array
+// uses two arrays and merges them together in the end
+func moveZeroesMerge(numbers []int) []int {
+	var nonZeroes []int
+	numbersLen := len(numbers)
+
+	for _, num := range numbers {
+		if num > 0 {
+			nonZeroes = append(nonZeroes, num)
+		}
+	}
+
+	// how many are left?
+	remainingNums := numbersLen - len(nonZeroes)
+
+	// make a new array with the remaining
+	zeroes := make([]int, remainingNums)
+
+	// now merge them
+	finalNumbers := append(nonZeroes, zeroes...)
 
 	return finalNumbers
 }
