347bbfb05bb1c57a3c8c8da847606fb12003a87c
Ryan Benson <benson.ryan@gmail.com>
2020-08-20 16:59:49 -0500

    Adds test for new moving zeroes with merging arrays

--- move-zeroes/move-zeroes_test.go
+++ move-zeroes/move-zeroes_test.go
@@ -22,8 +22,32 @@ func TestMoveZeroes_Success(t *testing.T) {
 	}
 }
 
+func TestMoveZeroesMerge_Success(t *testing.T) {
+	result := moveZeroesMerge([]int{1, 2, 3, 0, 4, 5, 0, 6})
+	expected := []int{1, 2, 3, 4, 5, 6, 0, 0}
+
+	allMatched := true
+	for i, expectedVal := range expected {
+		resultVal := result[i]
+		// if any fail, then it will error out
+		if expectedVal != resultVal {
+			allMatched = false
+		}
+	}
+	// anything not found? something went wrong
+	if allMatched == false {
+		t.Errorf("When moving zeroes on an array, incorrect result given, got: %v, expected: %v", result, expected)
+	}
+}
+
 func BenchmarkMoveZeroes(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		moveZeroes([]int{1, 2, 3, 0, 4, 5, 0, 6})
+	}
+}
+
+func BenchmarkMoveZeroesMerge(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		moveZeroesMerge([]int{1, 2, 3, 0, 4, 5, 0, 6})
 	}
 }
