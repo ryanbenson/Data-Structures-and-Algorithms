887590c1c0d242a4a7cb95dab94f0995f778f243
Ryan Benson <benson.ryan@gmail.com>
2020-08-27 16:56:22 -0500

    Starts implementation of wallet combo

--- wallet-combo/wallet-combo.go
+++ wallet-combo/wallet-combo.go
@@ -1,9 +1,31 @@
 package walletcombo
 
+import "fmt"
+
 func getCombo(menu []struct {
 	value int
 	name  string
 }, wallet int) [][]string {
 	var allCombos [][]string
+
+	for index, item := range menu {
+		combo := []string{}
+		currentComboCost := 0
+
+		for j := index; j < len(menu); j++ {
+			currentComboCost += item.value
+			if currentComboCost <= wallet {
+				combo = append(combo, item.name)
+			}
+			if currentComboCost >= wallet {
+				continue
+			}
+		}
+
+		allCombos = append(allCombos, combo)
+	}
+
+	fmt.Println(allCombos)
+
 	return allCombos
 }
