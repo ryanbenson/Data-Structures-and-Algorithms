bda7c07703cbdcf999355418377c4b82e1b50761
Ryan Benson <benson.ryan@gmail.com>
2020-09-03 16:23:43 -0500

    Adds psuedo code notes on the wallet combo exercise

--- wallet-combo/wallet-combo.go
+++ wallet-combo/wallet-combo.go
@@ -8,6 +8,13 @@ func getCombo(menu []struct {
 }, wallet int) [][]string {
 	var allCombos [][]string
 
+	// start at the beginning of the menu
+	// for each one, go through the entire menu, starting from 0
+	// and keep collecting combinations until 0 has no more
+	// then move to starting at 1, then 2...
+	// once you're at the end of the menu, move the first loop up 1
+	// keep going until you're at the end of the menu on the first loop
+
 	for index, item := range menu {
 		combo := []string{}
 		currentComboCost := 0
