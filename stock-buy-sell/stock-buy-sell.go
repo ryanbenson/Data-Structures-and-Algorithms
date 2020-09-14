10606b2c2da5c78481cfc5013bfa11d3e3579dd0
Ryan Benson <benson.ryan@gmail.com>
2020-09-14 16:46:11 -0500

    Adds implementation for get dates for buy and sell stock

--- stock-buy-sell/stock-buy-sell.go
+++ stock-buy-sell/stock-buy-sell.go
@@ -1,5 +1,22 @@
 package stockbuysell
 
+import "fmt"
+
+// returns the instructions on what days to buy the stock and when to sell it
 func getDates(days []int) string {
-	return ""
+	highestProfit := 0
+	highestProfitBuy := 0
+	highestProfitSell := 0
+
+	for i, day := range days {
+		for k := i; k < len(days); k++ {
+			profit := days[k] - day
+			if profit > highestProfit {
+				highestProfit = profit
+				highestProfitBuy = i + 1  // storing 1 based, not array 0 based
+				highestProfitSell = k + 1 // storing 1 based, not array 0 based
+			}
+		}
+	}
+	return fmt.Sprintf("buy on day %v, sell on day %v", highestProfitBuy, highestProfitSell)
 }
