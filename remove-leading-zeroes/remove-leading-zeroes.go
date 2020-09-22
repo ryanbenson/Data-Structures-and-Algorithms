e1c2cd2bcb13852f41c3215e5fb76339f21c0b8f
Ryan Benson <benson.ryan@gmail.com>
2020-09-22 15:49:44 -0500

    Adds implementation for removing leading zeroes using search

--- remove-leading-zeroes/remove-leading-zeroes.go
+++ remove-leading-zeroes/remove-leading-zeroes.go
@@ -1,5 +1,12 @@
 package removeleadingzeroes
 
-func remove(list []int) []int {
-	return []int{1, 0, 2, 3}
+import (
+	"sort"
+)
+
+// returns the list of numbers without all leading zeroes
+// zeroes after a n>=1 is fine
+func removeSearchList(list []int) []int {
+	index := sort.Search(len(list), func(i int) bool { return list[i] > 0 })
+	return list[index:len(list)]
 }
