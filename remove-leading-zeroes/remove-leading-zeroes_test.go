21cd30b0f95da5262e4b6772bb2b98558cdce246
Ryan Benson <benson.ryan@gmail.com>
2020-09-23 19:13:49 -0500

    Adds tests for for loop style of removing lead zeroes

--- remove-leading-zeroes/remove-leading-zeroes_test.go
+++ remove-leading-zeroes/remove-leading-zeroes_test.go
@@ -20,8 +20,29 @@ func TestRemoveSearchList(t *testing.T) {
 	}
 }
 
+func TestRemoveForLoop(t *testing.T) {
+	result := removeForLoop([]int{0, 0, 0, 1, 0, 2, 3})
+	expected := []int{1, 0, 2, 3}
+
+	if len(result) != len(expected) {
+		t.Errorf("When removing leading zeroes, incorrect length given, got: %v, expected: %v", len(result), len(expected))
+		return
+	}
+
+	if reflect.DeepEqual(result, expected) == false {
+		t.Errorf("When removing leading zeroes, incorrect values given, got: %v, expected: %v", result, expected)
+		return
+	}
+}
+
 func BenchmarkRemoveSearchList(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		removeSearchList([]int{0, 0, 0, 1, 0, 2, 3})
+	}
+}
+
+func BenchmarkRemoveForLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		removeForLoop([]int{0, 0, 0, 1, 0, 2, 3})
 	}
 }
