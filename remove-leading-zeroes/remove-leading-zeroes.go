48602c0e7db115638563bcf3bc6da8791229b64c
Ryan Benson <benson.ryan@gmail.com>
2020-09-23 19:12:18 -0500

    Adds removal via for loop

--- remove-leading-zeroes/remove-leading-zeroes.go
+++ remove-leading-zeroes/remove-leading-zeroes.go
@@ -10,3 +10,17 @@ func removeSearchList(list []int) []int {
 	index := sort.Search(len(list), func(i int) bool { return list[i] > 0 })
 	return list[index:len(list)]
 }
+
+// returns the list of numbers without all leading zeroes
+// zeroes after n>=1 is fine, using a for loop
+func removeForLoop(list []int) []int {
+	index := 0
+	for i, num := range list {
+		if num > 0 {
+			index = i
+			break
+		}
+	}
+	return list[index:len(list)]
+}
+}
