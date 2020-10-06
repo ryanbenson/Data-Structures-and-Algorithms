45a5c732e0ba0e60de369720f92a8fb0f4f3b28e
Ryan Benson <benson.ryan@gmail.com>
2020-10-06 10:30:44 -0500

    Fixes pizza party test and adds benchmark

--- pizza-party/pizza-party_test.go
+++ pizza-party/pizza-party_test.go
@@ -4,11 +4,44 @@ import "testing"
 
 func TestGetPizzaCount(t *testing.T) {
 	people := []Person{}
+	people = append(people, Person{
+		name: "Joe",
+		num:  9,
+	})
+	people = append(people, Person{
+		name: "Cami",
+		num:  3,
+	})
+	people = append(people, Person{
+		name: "Cassidy",
+		num:  4,
+	})
+
 	slicesPerPizza := 8
 	result := getPizzaCount(people, slicesPerPizza)
-	expected := 3
+	expected := 2
 
 	if result != expected {
 		t.Errorf("When getting count of pizzas to get, got %v, want, %v", result, expected)
+	}
+}
+
+func BenchmarkGetPizzaCount(b *testing.B) {
+	people := []Person{}
+	people = append(people, Person{
+		name: "Joe",
+		num:  9,
+	})
+	people = append(people, Person{
+		name: "Cami",
+		num:  3,
+	})
+	people = append(people, Person{
+		name: "Cassidy",
+		num:  4,
+	})
+	slicesPerPizza := 8
+	for i := 0; i < b.N; i++ {
+		getPizzaCount(people, slicesPerPizza)
 	}
 }
