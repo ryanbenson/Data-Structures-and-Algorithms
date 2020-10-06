5f2126b585b406fe6f70cab68c045616879cb689
Ryan Benson <benson.ryan@gmail.com>
2020-10-06 10:31:00 -0500

    Adds effective implementation for pizza party

--- pizza-party/pizza-party.go
+++ pizza-party/pizza-party.go
@@ -1,10 +1,17 @@
 package pizzaparty
 
+import "math"
+
 type Person struct {
 	name string
 	num  int
 }
 
 func getPizzaCount(people []Person, slices int) int {
-	return 1
+	totalSlicesNeeded := 0
+	for _, person := range people {
+		totalSlicesNeeded += person.num
+	}
+	totalPizzas := math.Ceil(float64(totalSlicesNeeded / slices))
+	return int(totalPizzas)
 }
