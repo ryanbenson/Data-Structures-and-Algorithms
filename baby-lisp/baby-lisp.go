23280ff31710fe8771d3dc4bbc6671f0d7ef2bdf
Ryan Benson <benson.ryan@gmail.com>
2020-10-20 10:04:03 -0500

    Adds some error handling for equation parsing

--- baby-lisp/baby-lisp.go
+++ baby-lisp/baby-lisp.go
@@ -1,5 +1,26 @@
 package babylisp
 
-func process(equation string) int {
-	return 0
+import "errors"
+
+func process(equation string) (int, error) {
+	if len(equation) == 0 {
+		return 0, nil
+	}
+
+	if len(equation) == 1 {
+		return 0, errors.New("Equation too short")
+	}
+
+	openChar := "("
+	closeChar := ")"
+
+	if string(equation[0]) != openChar {
+		return 0, errors.New("Invalid start of equation")
+	}
+
+	if string(equation[len(equation)-1]) != closeChar {
+		return 0, errors.New("Invalid end of equation")
+	}
+
+	return 0, nil
 }
