cc51b29e46472acff4ce04b85149fd9f169dbefb
Ryan Benson <benson.ryan@gmail.com>
2020-10-28 16:50:08 -0500

    Adds comments to our baby lisp implementation

--- baby-lisp/baby-lisp.go
+++ baby-lisp/baby-lisp.go
@@ -6,17 +6,23 @@ import (
 	"strings"
 )
 
+// the list of possible operations
 var operations = []string{"add", "subtract", "multiply", "divide"}
 
+// Takes a lispy string that's a simple calculation and processes it
 func process(equation string) (int, error) {
+	// if an equation has no length
 	if len(equation) == 0 {
 		return 0, nil
 	}
 
-	if len(equation) == 1 {
+	// smallest equation size "(add 1 1)"
+	minEquationSize := 9
+	if len(equation) < minEquationSize {
 		return 0, errors.New("Equation too short")
 	}
 
+	// check that the equation is wrapped in ()
 	openChar := "("
 	closeChar := ")"
 
@@ -28,13 +34,17 @@ func process(equation string) (int, error) {
 		return 0, errors.New("Invalid end of equation")
 	}
 
+	// take everything in between the ()
 	eq := equation[1 : len(equation)-1]
+	// split the string by spaces
 	elements := strings.Fields(eq)
 
+	// if we have more or less than 3 items, then the equation is invalid
 	if len(elements) != 3 {
 		return 0, errors.New("Invalid number of equation elements")
 	}
 
+	// validate the operand
 	operand := elements[0]
 	if isValidOperand(operand, operations) == false {
 		return 0, errors.New("Invalid operand")
@@ -42,6 +52,7 @@ func process(equation string) (int, error) {
 
 	result := 0
 
+	// convert the numbers into ints, check for any issues
 	x, err := strconv.Atoi(elements[1])
 	y, err := strconv.Atoi(elements[2])
 
@@ -65,6 +76,7 @@ func process(equation string) (int, error) {
 	return result, nil
 }
 
+// Determines if the operand provided is in the whitelist
 func isValidOperand(operand string, operations []string) bool {
 	for _, validOperand := range operations {
 		if validOperand == operand {
