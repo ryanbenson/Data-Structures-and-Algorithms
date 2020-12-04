344abe13a06daa6118d4cb3143baff7dd71e9bfb
Ryan Benson <benson.ryan@gmail.com>
2020-12-04 12:18:48 -0600

    Adds more testing for baby lisp

--- baby-lisp/baby-lisp_test.go
+++ baby-lisp/baby-lisp_test.go
@@ -5,9 +5,48 @@ import (
 	"testing"
 )
 
-func TestProcess(t *testing.T) {
+func TestProcessAdd(t *testing.T) {
 	result, err := process("(add 1 2)")
 	expected := 3
+
+	if result != expected {
+		t.Errorf("When processing a calculation, got: %v, want: %v.", result, expected)
+	}
+
+	if err != nil {
+		t.Errorf("When processing a calculation and checking the error, got: %v, want: %v.", err, nil)
+	}
+}
+
+func TestProcessSubtract(t *testing.T) {
+	result, err := process("(subtract 1 2)")
+	expected := -1
+
+	if result != expected {
+		t.Errorf("When processing a calculation, got: %v, want: %v.", result, expected)
+	}
+
+	if err != nil {
+		t.Errorf("When processing a calculation and checking the error, got: %v, want: %v.", err, nil)
+	}
+}
+
+func TestProcessDivide(t *testing.T) {
+	result, err := process("(divide 99 11)")
+	expected := 9
+
+	if result != expected {
+		t.Errorf("When processing a calculation, got: %v, want: %v.", result, expected)
+	}
+
+	if err != nil {
+		t.Errorf("When processing a calculation and checking the error, got: %v, want: %v.", err, nil)
+	}
+}
+
+func TestProcessMultiply(t *testing.T) {
+	result, err := process("(multiply 5 7)")
+	expected := 35
 
 	if result != expected {
 		t.Errorf("When processing a calculation, got: %v, want: %v.", result, expected)
@@ -44,7 +83,7 @@ func TestProcessEmpty(t *testing.T) {
 }
 
 func TestProcessMissingOpen(t *testing.T) {
-	result, err := process("add 1 2)")
+	result, err := process("subtract 1 2)")
 	expectedErr := errors.New("Invalid start of equation")
 
 	if result != 0 {
@@ -57,7 +96,7 @@ func TestProcessMissingOpen(t *testing.T) {
 }
 
 func TestProcessMissingClose(t *testing.T) {
-	result, err := process("(add 1 2")
+	result, err := process("(subtract 1 2")
 	expectedErr := errors.New("Invalid start of equation")
 
 	if result != 0 {
@@ -82,9 +121,35 @@ func TestProcessMissingElements(t *testing.T) {
 	}
 }
 
+func TestProcessTooManyElements(t *testing.T) {
+	result, err := process("(add 1 3 5 7 8 9 10)")
+	expectedErr := errors.New("Invalid number of equation elements")
+
+	if result != 0 {
+		t.Errorf("When processing an invalid string, got: %v, want: %v.", result, nil)
+	}
+
+	if err == nil {
+		t.Errorf("When processing an invalid string and checking the error, got: %v, want: %v.", err, expectedErr)
+	}
+}
+
 func TestProcessInvalidNumbers(t *testing.T) {
 	result, err := process("(add 1 hello)")
 	expectedErr := errors.New("Invalid numbers provided")
+
+	if result != 0 {
+		t.Errorf("When processing an invalid string, got: %v, want: %v.", result, nil)
+	}
+
+	if err == nil {
+		t.Errorf("When processing an invalid string and checking the error, got: %v, want: %v.", err, expectedErr)
+	}
+}
+
+func TestProcessInvalidOperand(t *testing.T) {
+	result, err := process("(testing 1 hello)")
+	expectedErr := errors.New("Invalid operand")
 
 	if result != 0 {
 		t.Errorf("When processing an invalid string, got: %v, want: %v.", result, nil)
