0c10e2093d7e4d502755ed10c3432369208d278d
Ryan Benson <benson.ryan@gmail.com>
2020-12-07 15:09:20 -0600

    Adds solution for day 1 for advent of code

--- advent-code-report-repair/report-repair.go
+++ advent-code-report-repair/report-repair.go
@@ -3,6 +3,24 @@ package reportrepair
 // provide a list of ints where two numbers will
 // sum to 2020, using those two numbers,
 // multiply them together. that's the return
-func repair([]int) int {
-	return 1
+func repair(numbers []int) int {
+	x := 0
+	y := 0
+	totalNumbers := len(numbers)
+	goal := 2020
+
+	for i, num := range numbers {
+		// don't bother going back in the list of numbers, start at i
+		for k := i; k < totalNumbers; k++ {
+			sum := num + numbers[k]
+			if sum == goal {
+				x = num
+				y = numbers[k]
+				break
+			}
+		}
+	}
+
+	result := x * y
+	return result
 }
