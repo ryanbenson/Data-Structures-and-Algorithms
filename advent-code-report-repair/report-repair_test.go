99ee12a9e2b3567a9681f516f042f2f32cbeb720
Ryan Benson <benson.ryan@gmail.com>
2020-12-07 15:00:27 -0600

    Updates example answer

--- advent-code-report-repair/report-repair_test.go
+++ advent-code-report-repair/report-repair_test.go
@@ -7,7 +7,7 @@ import (
 func TestProcessExample(t *testing.T) {
 	list := []int{1721, 979, 366, 299, 675, 1456}
 	result := repair(list)
-	expected := 1
+	expected := 514579
 
 	if result != expected {
 		t.Errorf("When repairing a report, got: %v, want: %v.", result, expected)
