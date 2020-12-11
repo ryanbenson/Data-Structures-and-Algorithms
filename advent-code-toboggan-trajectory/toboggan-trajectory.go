b995cc392fc661475aaf8a82df59de322fb1e5ec
Ryan Benson <benson.ryan@gmail.com>
2020-12-11 15:33:24 -0600

    Adds implementation for toboggan trajectory

--- advent-code-toboggan-trajectory/toboggan-trajectory.go
+++ advent-code-toboggan-trajectory/toboggan-trajectory.go
@@ -1,5 +1,44 @@
 package toboggantrajectory
 
+import (
+	"strings"
+)
+
 func findTrees(mapContent string) int {
-	return 0
+	mapData := strings.Split(mapContent, "\n")
+
+	right := 3
+	down := 1
+	tree := "#"
+
+	totalLines := len(mapData)
+	lineLimit := len(mapData[0])
+	currentLine := 0
+	curLinePos := 0
+	totalTrees := 0
+
+	for {
+		// go right
+		curLinePos = curLinePos + right
+		// if we reach the end of the right side of the map
+		// then get the remainder and restart since it repeats
+		if curLinePos > lineLimit {
+			curLinePos = curLinePos - lineLimit
+		}
+
+		// go down the map
+		currentLine = currentLine + down
+		// if we reach the end of the map, exit
+		if currentLine >= totalLines {
+			break
+		}
+
+		// did we find a tree?
+		mapItem := string(mapData[currentLine][curLinePos])
+		if mapItem == tree {
+			totalTrees = totalTrees + 1
+		}
+	}
+
+	return totalTrees
 }
