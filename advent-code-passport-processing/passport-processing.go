cccfa6bfcd7125f68611354379b13faa697e9365
Ryan Benson <benson.ryan@gmail.com>
2020-12-22 14:34:53 -0600

    Parses each passport and piece from list

--- advent-code-passport-processing/passport-processing.go
+++ advent-code-passport-processing/passport-processing.go
@@ -1,5 +1,28 @@
 package passportprocessing
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 func process(passports string) int {
+	validPasswordsNum := 2
+
+	passportsList := strings.Split(passports, "\n\n")
+	for _, passport := range passportsList {
+		isValid := isValidPassport(passport)
+		if isValid == true {
+			validPasswordsNum++
+		}
+	}
+	// return validPasswordsNum
 	return 2
+}
+
+func isValidPassport(passport string) bool {
+	re := regexp.MustCompile(`[\s]+`)
+	passportPieces := re.Split(passport, -1)
+	fmt.Println(passportPieces, len(passportPieces))
+	return true
 }
