e29c91ae92f416da32648ed69ce361d0f17fb279
Ryan Benson <benson.ryan@gmail.com>
2021-01-07 13:11:58 -0600

    Parses boarding passes

--- advent-code-binary-boarding/binary-boarding.go
+++ advent-code-binary-boarding/binary-boarding.go
@@ -1,5 +1,27 @@
 package binaryboarding
 
-func getMaxId(passes []string) int {
+import (
+	"fmt"
+	"strings"
+)
+
+func getMaxId(passes string) int {
+	maxId := 0
+	passList := strings.Split(passes, "\n")
+	for _, pass := range passList {
+		id := getSeatId(pass)
+		if id > maxId {
+			maxId = id
+		}
+	}
+	return maxId
+}
+
+func getSeatId(pass string) int {
+	// totalRows := 128 // 0-127
+	letters := strings.Split(pass, "")
+	for _, letter := range letters {
+		fmt.Println(letter)
+	}
 	return 0
 }
