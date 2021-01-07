fcc6d1629e6f1a45a74558535517be95239ab3aa
Ryan Benson <benson.ryan@gmail.com>
2021-01-07 13:30:12 -0600

    Updates function in binary boarding to return row, column and id

--- advent-code-binary-boarding/binary-boarding.go
+++ advent-code-binary-boarding/binary-boarding.go
@@ -9,7 +9,7 @@ func getMaxId(passes string) int {
 	maxId := 0
 	passList := strings.Split(passes, "\n")
 	for _, pass := range passList {
-		id := getSeatId(pass)
+		row, column, id := getSeat(pass)
 		if id > maxId {
 			maxId = id
 		}
@@ -17,11 +17,11 @@ func getMaxId(passes string) int {
 	return maxId
 }
 
-func getSeatId(pass string) int {
+func getSeat(pass string) (int, int, int) {
 	// totalRows := 128 // 0-127
 	letters := strings.Split(pass, "")
 	for _, letter := range letters {
 		fmt.Println(letter)
 	}
-	return 0
+	return 0, 0, 0
 }
