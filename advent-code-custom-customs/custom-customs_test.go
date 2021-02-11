9fcbcb09d3444ffdecbf1b98f923dc5019f7935c
Ryan Benson <benson.ryan@gmail.com>
2021-02-11 16:57:03 -0600

    Adds test sample for custom customs

--- advent-code-custom-customs/custom-customs_test.go
+++ advent-code-custom-customs/custom-customs_test.go
@@ -4,9 +4,25 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T) {
-	expected := 0
-	result := sum("hello")
+func TestSumSample(t *testing.T) {
+	expected := 11
+	input := `abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`
+
+	result := sum(input)
 
 	if result != expected {
 		t.Errorf("When getting custom customs, looking at sum, got: %v, want: %v.", result, expected)
