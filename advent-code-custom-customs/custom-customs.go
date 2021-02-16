fcf852ce0bb7bfb5a33d2f57ffa92b091fa8fd4b
Ryan Benson <benson.ryan@gmail.com>
2021-02-16 14:01:59 -0600

    Gets groups and row answers for custom customs

--- advent-code-custom-customs/custom-customs.go
+++ advent-code-custom-customs/custom-customs.go
@@ -1,5 +1,23 @@
 package customcustoms
 
+import (
+	"fmt"
+	"strings"
+)
+
 func sum(questions string) int {
+	rows := getRows(questions)
+	for _, row := range rows {
+		rowAnswers := getRowAnswers(row)
+		fmt.Println(rowAnswers)
+	}
 	return 0
+}
+
+func getRows(questions string) []string {
+	return strings.Split(questions, "\n\n")
+}
+
+func getRowAnswers(row string) []string {
+	return strings.Split(row, "\n")
 }
