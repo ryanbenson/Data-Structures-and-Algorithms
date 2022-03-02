f5a0fddcc94ee44bafde2b578df78a821826c7cc
Ryan Benson <benson.ryan@gmail.com>
2022-03-01 18:18:26 -0600

    Outlines the process to figuring out the largest rect

--- largest-island/largest-island.go
+++ largest-island/largest-island.go
@@ -1,6 +1,19 @@
 package largestisland
 
 // largestRect finds the largest rectangle island
-func largestRect(arr [][]int) (string) {
+func largestRect(island  [][]int) string {
+	// go through each row, if you find a 1
+	// see if the next item in the row is a 1,
+	// or the next item in the column is a 1,
+	// if so, run a separate function to find the size
+	// keep track of the largest size. is the new rect bigger?
+	// if so, replace, and keep going
 	return "2x2"
+}
+
+func findRectSize(x int, y int) (string, int) {
+	// go through the column to find the last 1
+	// go through the row to find the last 1
+	// return the size in a string (e.g. "2x2")
+	// also return the full size (e.g. 4)
 }
