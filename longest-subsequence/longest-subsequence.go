2c9cc376849282bf44e405c34202f1bd4cc8531f
Ryan Benson <benson.ryan@gmail.com>
2022-03-07 17:37:53 -0600

    Adds comment, sorts list of numbers

--- longest-subsequence/longest-subsequence.go
+++ longest-subsequence/longest-subsequence.go
@@ -1,5 +1,12 @@
 package longestsubsequence
 
-func longestSubSeq(islands []int) int {
+import (
+	"sort"
+)
+
+// finds the longest subsequence of numbers within a list of numbers
+// providing the length of the longest subsequence found
+func longestSubSeq(sequence []int) int {
+	sort.Ints(sequence)
 	return 5
 }
