6e3a9e3ca416571be52843abc038490bd5bac84d
Ryan Benson <benson.ryan@gmail.com>
2022-03-11 18:28:11 -0600

    Adds garden planting implementation

--- garden-plant/can-plant.go
+++ garden-plant/can-plant.go
@@ -1,5 +1,29 @@
 package canplant
 
+import "math"
+
 func canPlant(garden []int, plants int) bool {
-	return true
+	totalAvailableSpots := 0
+	currentAvailableSpotsLen := 0
+	for _, spot := range garden {
+		if spot == 0 {
+			currentAvailableSpotsLen++
+		}
+		if spot == 1 {
+			// easier if the spots are even since each plant needs an open space around it
+			// if it's even, take away one
+			if currentAvailableSpotsLen %2 == 0 {
+				currentAvailableSpotsLen = currentAvailableSpotsLen - 1
+			}
+			plantableSpotsFound := math.Ceil(float64(currentAvailableSpotsLen) / float64(3))
+			totalAvailableSpots = totalAvailableSpots + int(plantableSpotsFound)
+			// reset so we can keep going
+			currentAvailableSpotsLen = 0
+		}
+	}
+	if plants <= totalAvailableSpots {
+		return true
+	}
+
+	return false
 }
