aad57da58859bdfe907c4ad7137ff82a19638259
Ryan Benson <benson.ryan@gmail.com>
2022-03-12 10:08:12 -0600

    Adds more tests for can plant in a garden

--- garden-plant/can-plant_test.go
+++ garden-plant/can-plant_test.go
@@ -15,10 +15,32 @@ func TestCanPlant(t *testing.T) {
 	}
 }
 
-func BenchmarkLargestRect(b *testing.B) {
+func TestCanPlantTooManyDesired(t *testing.T) {
+	garden := []int{1, 0, 0, 0, 1}
+	plantsDesired := 4
+	result := canPlant(garden, plantsDesired)
+	expected := false
+
+	if result != expected {
+		t.Errorf("When finding if all plants can be planted when there are too many, incorrect result given, got: %v, expected: %v", result, expected)
+	}
+}
+
+func TestCanPlantLong(t *testing.T) {
+	garden := []int{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1}
+	plantsDesired := 4
+	result := canPlant(garden, plantsDesired)
+	expected := true
+
+	if result != expected {
+		t.Errorf("When finding if lots of plants can be planted, incorrect result given, got: %v, expected: %v", result, expected)
+	}
+}
+
+func BenchmarkCanPlantLong(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		garden := []int{1, 0, 0, 0, 1}
-		plantsDesired := 1
+		garden := []int{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1}
+		plantsDesired := 4
 		canPlant(garden, plantsDesired)
 	}
 }
