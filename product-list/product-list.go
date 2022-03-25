8cc7009fb23f269422975216d1d367ce7c29b7b2
Ryan Benson <benson.ryan@gmail.com>
2022-03-25 10:52:05 -0500

    Adds implementatino for product list

--- product-list/product-list.go
+++ product-list/product-list.go
@@ -1,1 +1,29 @@
 package productlist
+
+import (
+	"errors"
+)
+
+type productList struct {
+	list []int
+}
+
+func (p *productList) add(item int) {
+	p.list = append(p.list, item)
+}
+
+func (p *productList) get(num int) (int, error) {
+	listLen := len(p.list)
+	if num > listLen {
+		return 0, errors.New("Invalid range of products. Exceeds amount of products in the list")
+	}
+
+	var startIndex int = 0
+	startIndex = listLen - num
+	desiredProducts := p.list[startIndex:]
+	totalProductVal := 1
+	for _, productVal := range desiredProducts {
+		totalProductVal *= productVal
+	}
+	return totalProductVal, nil
+}
--- product-list/product-list_test.go
+++ product-list/product-list_test.go
@@ -1,14 +1,25 @@
 package productlist
 
-// func TestProductlist(t *testing.T) {
-// 	times := []string{"01:00", "08:15", "11:30", "13:45", "14:10", "20:05"}
-// 	result := smallestTimeInterval(times, 60)
-// 	expected := "25 minutes"
+import "testing"
 
-// 	if result != expected {
-// 		t.Errorf("When finding the shortest time interval between a set of times, incorrect result given, got: %v, expected: %v", result, expected)
-// 	}
-// }
+func TestProductGet(t *testing.T) {
+	p := productList {}
+	p.add(7)
+	p.add(0)
+	p.add(2)
+	p.add(5)
+	p.add(4)
+	result, err := p.get(3)
+	expected := 40
+
+	if result != expected {
+		t.Errorf("When finding the product total value, incorrect result given, got: %v, expected: %v", result, expected)
+	}
+
+	if err != nil {
+		t.Errorf("When finding the product total value, got an error thrown: %v", err)
+	}
+}
 
 // func BenchmarkSmallestTimeIntervalSimple(b *testing.B) {
 // 	for i := 0; i < b.N; i++ {
