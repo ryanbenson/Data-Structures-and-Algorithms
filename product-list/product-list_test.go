3a10877d86b803a8b948c7289dc6f0c7d431dc06
Ryan Benson <benson.ryan@gmail.com>
2022-03-28 21:44:30 -0500

    Adds too many products requested test

--- product-list/product-list_test.go
+++ product-list/product-list_test.go
@@ -21,6 +21,25 @@ func TestProductGet(t *testing.T) {
 	}
 }
 
+func TestProductGetTooManyRequested(t *testing.T) {
+	p := productList {}
+	p.add(7)
+	p.add(0)
+	p.add(2)
+	p.add(5)
+	p.add(4)
+	result, err := p.get(10)
+	expected := 0
+
+	if result != expected {
+		t.Errorf("When getting too manys products, incorrect result given, got: %v, expected: %v", result, expected)
+	}
+
+	if err == nil {
+		t.Errorf("When gettinng too many products, got an error thrown: %v", err)
+	}
+}
+
 // func BenchmarkSmallestTimeIntervalSimple(b *testing.B) {
 // 	for i := 0; i < b.N; i++ {
 // 		times := []string{"01:00", "08:15", "11:30", "13:45", "14:10", "20:05"}
