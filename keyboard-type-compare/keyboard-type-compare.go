2b810f5a1260570cf7c7e1e5f86d7cbc598867e4
Ryan Benson <benson.ryan@gmail.com>
2022-04-08 21:06:25 -0500

    Adds more implementation to keyboard input

--- keyboard-type-compare/keyboard-type-compare.go
+++ keyboard-type-compare/keyboard-type-compare.go
@@ -1,5 +1,13 @@
 package keyboardtype
 
+// determines if two keyboard inputs are the same
 func isEqual(str1 string, str2 string) bool {
-	return true
+	finalStr1 := inputToString(str1)
+	finalStr2 := inputToString(str2)
+	return finalStr1 == finalStr2
+}
+
+// converts an input of keyboard character sequence to a final string
+func inputToString(input string) string {
+	return ""
 }
