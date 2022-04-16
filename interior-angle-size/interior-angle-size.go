7cf8c25f888e050e659f5917a9081310dcc55234
Ryan Benson <benson.ryan@gmail.com>
2022-04-15 21:57:28 -0500

    Checks for invalid size of a shape

--- interior-angle-size/interior-angle-size.go
+++ interior-angle-size/interior-angle-size.go
@@ -2,5 +2,8 @@ package interioranglesize
 
 // provides the interior angle of a same-size polygon with the given size
 func getInteriorAngle(size int) int {
+	if size < 2 {
+		return 0
+	}
 	return (size-2) * 180 / size
 }
