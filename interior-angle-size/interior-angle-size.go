a445d6bf325bb6eafba121ea006387de04722660
Ryan Benson <benson.ryan@gmail.com>
2022-04-20 15:21:14 -0500

    Adds exterior angle for polygon

--- interior-angle-size/interior-angle-size.go
+++ interior-angle-size/interior-angle-size.go
@@ -7,3 +7,11 @@ func getInteriorAngle(size int) int {
 	}
 	return (size-2) * 180 / size
 }
+
+// provides the exterior angle of a same-size polygon with the given size
+func getExteriorAngle(size int) float64 {
+	if size < 2 {
+		return 0
+	}
+	return 360 / float64(size)
+}
