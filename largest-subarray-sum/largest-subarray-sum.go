39ffdf346c66cd6c9b5b44f6603cc1bb0222c712
Ryan Benson <benson.ryan@gmail.com>
2022-04-27 15:29:56 -0500

    Adds check for subset count compared to list size

--- largest-subarray-sum/largest-subarray-sum.go
+++ largest-subarray-sum/largest-subarray-sum.go
@@ -1,10 +1,18 @@
 package largestsubarraysum
 
-func getSum([]int list, int subsetCount) []int {
+import (
+	"errors"
+)
+
+func getSum(list []int, subsetCount int) ([]int, error) {
 	// idea 1:
 	// go through the list, split it into smaller pieces by the subsetcount
 	// 1,2,3,4,5,6 | 3
 	// 1,2,3 | 2,3,4 | 3,4,5 | 4,5,6
 	// go through those and find the largest sum
-	return []int{}
+	listLength := len(list)
+	if listLength < subsetCount {
+		return nil, errors.New("Subset count cannot be larger than the list size")
+	}
+	return []int{}, nil
 }
