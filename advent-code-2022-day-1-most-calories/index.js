e261c6bd86e38899fd8af4b04880eb065f854d1c
Ryan Benson <benson.ryan@gmail.com>
2022-12-08 10:02:29 -0600

    Adds stream version for getting most calories

--- advent-code-2022-day-1-most-calories/index.js
+++ advent-code-2022-day-1-most-calories/index.js
@@ -30,3 +30,38 @@ const sample = `1000
 
 const result = getMostCalories(sample);
 console.log(result);
+
+const fs = require('fs');
+const readline = require('readline');
+const getMostCaloriesFile = (file) => {
+    let mostCalories = 0;
+    let currentPersonCalories = 0;
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: fs.createReadStream(file),
+            crlfDelay: Infinity
+        });
+        rl.on('line', (line) => {
+            const input = Buffer.from(line).toString();
+            if (input.length === 0) {
+                if (currentPersonCalories > mostCalories) {
+                    mostCalories = currentPersonCalories;
+                    currentPersonCalories = 0;
+                }
+            } else {
+                currentPersonCalories += Number(input);
+            }
+        });
+        rl.on('error', (e) => {
+            reject(e);
+        });
+        rl.on('close', () => {
+            resolve(mostCalories);
+        });
+    });
+};
+
+(async () => {
+    const result = await getMostCaloriesFile('sample.txt');
+    console.log(result);
+})();
