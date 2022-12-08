7fb0849e317624d8fa3d10f8195f5affbd68603c
Ryan Benson <benson.ryan@gmail.com>
2022-12-08 12:00:11 -0600

    Adds comments and cleanup to 2022 advent code day 1

--- advent-code-2022-day-1-most-calories/index.js
+++ advent-code-2022-day-1-most-calories/index.js
@@ -1,6 +1,17 @@
+/**
+ * Finds the person with the most snack calories
+ * Input will be a string with every line has a snack value of calories in a number
+ * If there's a blank line, then that separates a person from one another
+ * This will find the person that has the most calories with their snacks
+ * @param {String} file 
+ * @returns {Number}
+ */
 const getMostCalories = (input) => {
     let mostCalories = 0;
+    // split it by new lines, if there's a gap, it separates people
     const peopleWithItems = input.split('\n\n');
+    // each single line separates their snacks, add them up
+    // and if their calorie count exceeds the current top, replace it
     peopleWithItems.forEach((items) => {
         const itemsList = items.split('\n');
         const total = itemsList.reduce((cur, next) => {
@@ -13,36 +24,29 @@ const getMostCalories = (input) => {
     return mostCalories;
 };
 
-const sample = `1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000`;
-
-const result = getMostCalories(sample);
-console.log(result);
-
 const fs = require('fs');
 const readline = require('readline');
+/**
+ * Finds the person with the most snack calories
+ * Input will be a file with every line has a snack value of calories in a number
+ * If there's a blank line, then that separates a person from one another
+ * This will find the person that has the most calories with their snacks
+ * @param {String} file 
+ * @returns {Number}
+ */
 const getMostCaloriesFile = (file) => {
     let mostCalories = 0;
     let currentPersonCalories = 0;
+    // return a promise so we can a final value from the stream
     return new Promise((resolve, reject) => {
         const rl = readline.createInterface({
             input: fs.createReadStream(file),
             crlfDelay: Infinity
         });
         rl.on('line', (line) => {
+            // convert the stream buffer to a string
             const input = Buffer.from(line).toString();
+            // we've reached the end of their snacks, determine if their calorie count is the top, and reset
             if (input.length === 0) {
                 if (currentPersonCalories > mostCalories) {
                     mostCalories = currentPersonCalories;
@@ -61,7 +65,7 @@ const getMostCaloriesFile = (file) => {
     });
 };
 
-(async () => {
-    const result = await getMostCaloriesFile('sample.txt');
-    console.log(result);
-})();
+module.exports = {
+    getMostCalories,
+    getMostCaloriesFile
+};
--- advent-code-2022-day-1-most-calories/index.test.js
+++ advent-code-2022-day-1-most-calories/index.test.js
@@ -1,10 +1,28 @@
 const test = require('ava');
+const { getMostCalories, getMostCaloriesFile } = require('./index')
 
-test('foo', t => {
-	t.pass();
+test('#getMostCalories when given a valid sample', t => {
+    const sample = `1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000`;
+    const result = getMostCalories(sample);
+    const expected = 24000;
+    t.is(result, expected);
 });
 
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
+test('#getMostCaloriesFile when given a valid sample file', async t => {
+    const result = await getMostCaloriesFile('./advent-code-2022-day-1-most-calories/sample.txt');
+    const expected = 24000;
+    t.is(result, expected);
 });
