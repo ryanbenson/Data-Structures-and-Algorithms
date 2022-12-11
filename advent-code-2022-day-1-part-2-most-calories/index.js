3d6506c8e168a7d727544d8ed71a0cd09029a58e
Ryan Benson <benson.ryan@gmail.com>
2022-12-11 09:38:19 -0600

    Adds variation to get calories for day 1 part 2 in advent 2022

--- advent-code-2022-day-1-part-2-most-calories/index.js
+++ advent-code-2022-day-1-part-2-most-calories/index.js
@@ -28,7 +28,48 @@ const getCalories = (input) => {
     return totalCalories;
 };
 
+/**
+ * Finds the total calories of the top 3 people with the most snack calories
+ * Input will be a string with every line has a snack value of calories in a number
+ * If there's a blank line, then that separates a person from one another
+ * This will find the person that has the most calories with their snacks
+ * @param {String} input to process 
+ * @returns {Number}
+ */
+const getCaloriesRanked = (input) => {
+    let topCalories1 = 0;
+    let topCalories2 = 0;
+    let topCalories3 = 0;
+    // split it by new lines, if there's a gap, it separates people
+    const peopleWithItems = input.split('\n\n');
+    // each single line separates their snacks, add them up
+    // and if their calorie count exceeds the current top, replace it
+    peopleWithItems.forEach((items) => {
+        const itemsList = items.split('\n');
+        const total = itemsList.reduce((cur, next) => {
+            return cur + Number(next);
+        }, 0);
+        if (total > topCalories1) {
+            // de-rank the current list, then update with the new one
+            topCalories3 = topCalories2;
+            topCalories2 = topCalories1;
+            topCalories1 = total;
+        } else if(total > topCalories2) {
+            topCalories3 = topCalories2;
+            topCalories2 = total;
+        } else if(total > topCalories3) {
+            // nothing to de-rank, just overwrite
+            topCalories3 = total;
+        }
+    });
+    
+    const totalCalories = topCalories1 + topCalories2 + topCalories3;
+    return totalCalories;
+};
+
+
 
 module.exports = {
-    getCalories
+    getCalories,
+    getCaloriesRanked
 };
--- advent-code-2022-day-1-part-2-most-calories/readme.md
+++ advent-code-2022-day-1-part-2-most-calories/readme.md
@@ -5,5 +5,12 @@
 https://adventofcode.com/2022/day/1#part2
 
 ```bash
-#getCalories x 782,756 ops/sec ±0.98% (94 runs sampled)
+#getCalories x 754,125 ops/sec ±1.50% (90 runs sampled)
+#getCaloriesRanked x 826,980 ops/sec ±1.75% (94 runs sampled)
+#getCaloriesLargeDataset x 4,096 ops/sec ±1.65% (89 runs sampled)
+#getCaloriesRankedLargeDataset x 4,819 ops/sec ±1.71% (88 runs sampled)
 ```
+
+## Notes
+
+Doing the ranking manually is fairly more efficient for time and memory compared to maintaining an array. It's about 15-17% more efficient with large data sets, and around 10% more efficient with smaller ones.
