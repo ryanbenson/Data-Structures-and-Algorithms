cd6a20557622bd0c3e31941da3bde73e1df7189f
Ryan Benson <benson.ryan@gmail.com>
2024-04-10 21:02:25 -0500

    Adds remove element exercise

--- leetcode-27-remove-element/index.js
+++ leetcode-27-remove-element/index.js
@@ -1,4 +1,13 @@
 /**
+ * Essentially we just loop through each item, if we find
+ * a match, move it to the end because the response wants
+ * all matches at the end. We actually swap the last item
+ * with the current one that gets replaced. Then we
+ * reduce the window size because we "removed" an item.
+ *
+ * This also keeps the Big O at:
+ * Time: O(n) because we iterate through the entire array once.
+ * Space: O(1) because we are not using any extra space that grows with the input size.
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
