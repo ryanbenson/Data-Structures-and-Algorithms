70098cb11b700bdcb9477842d0eee48c0f9384f9
Ryan Benson <benson.ryan@gmail.com>
2024-04-15 18:28:48 -0500

    Adds comment to jump game

--- leetcode-55-jump-game/index.js
+++ leetcode-55-jump-game/index.js
@@ -3,6 +3,8 @@ var canJump = function (nums) {
   let reachable = 0;
 
   for (let index = 0; index < nums.length && index <= reachable; index++) {
+    // compare your current number+index vs the next number+index, take the largest (greedy)
+    // if the new one wins, update reachable
     reachable = Math.max(reachable, index + nums[index]);
     if (reachable >= target) return true;
   }
