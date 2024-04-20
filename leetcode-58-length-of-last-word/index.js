e6df72603d91b58e8b17c90742afbbd82636b364
Ryan Benson <benson.ryan@gmail.com>
2024-04-19 21:12:54 -0500

    Adds looping method for length of last word

--- leetcode-58-length-of-last-word/index.js
+++ leetcode-58-length-of-last-word/index.js
@@ -13,3 +13,28 @@ var lengthOfLastWord = function (s) {
   var words = s.split(" ");
   return words[words.length - 1].length;
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWordLoop = function (s) {
+  s = s.trim();
+  if (s.length === 0) {
+    return 0;
+  }
+  if (s.length === 1) {
+    return 1;
+  }
+  var spaceI = 0;
+  for (var i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " ") {
+      spaceI = i;
+      break;
+    }
+  }
+  if (spaceI === 0) {
+    return s.length;
+  }
+  return s.length - 1 - spaceI;
+};
